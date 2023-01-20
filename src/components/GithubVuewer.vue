79c3946f0dade8ef4303c8791fd5350cba7c9968
Fernanda Liz <fernandaliz2004@hotmail.com>
2023-01-20 10:24:07 -0300

    add github vuewer

---
+++ src/components/GithubVuewer.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <github-repos/>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  import GithubRepos from './GithubRepos'
+
+  export default {
+  components: {
+    GithubRepos
+  },
+    data: () => ({
+    
+    }),
+  }
+</script>
