3f2b9e4c7582c65605e165706ed87798f96c727c
Fernanda Liz <fernandaliz2004@hotmail.com>
2023-01-23 19:57:57 -0300

    busca todos o repos do usuario

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,34 @@
+async function fetch_all_pages(url){
+  let nextPage = true
+  let result = []
+  let page = 1
+  while(nextPage){
+      const response = await fetch(`${url}?page=${page}`)
+      const tmpitems = await response.json()
+      if(tmpitems.length > 0){
+          result = result.concat(tmpitems)
+          page++
+      } else {
+          nextPage = false
+      }
+  }
+  return result
+}
+
 export const api = {
-  async search_users(searchstring){
+  async searchUsers(searchstring){
     const url = `https://api.github.com/search/users?q=${searchstring}`
-    const result = await fetch(url)
-    return await result.json()
+    const response = await fetch(url)
+    return await response.json()
   },
-  async list_repos(username, page){
-    if (!page){
-      page = 1
-    }
-    const url = `https://api.github.com/users/${username}/repos?page=${page}`
-    const result = await fetch(url)
-    return await result.json()
+  async listRepos(username){
+    const url = `https://api.github.com/users/${username}/repos`
+    const data = await fetch_all_pages(url)
+    return data
+  },
+  async listIssues(owner, name, page) {
+    const url = `https://api.github.com/repos/${owner}/${name}/issues?page=${page}`
+    const response = await fetch(url)
+    return await response.json()
   }
 }
